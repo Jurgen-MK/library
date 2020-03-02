1723aebbdda4de07e268b76c8839e45b4af635c3
Моторнов Кирилл <Motornov_K@railways.kz>
2020-03-02 11:54:00 +0600

    tests

--- lib-auth-jwt/src/test/java/kz/ktzh/UserServiceTest.java
+++ lib-auth-jwt/src/test/java/kz/ktzh/UserServiceTest.java
@@ -1,12 +1,20 @@
 package kz.ktzh;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -14,9 +22,19 @@ import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.NonTransientDataAccessException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import kz.ktzh.models.UserGroupsRepository;
+import kz.ktzh.models.UserInfo;
+import kz.ktzh.models.UserInfoRepository;
+import kz.ktzh.models.UserRepository;
+import kz.ktzh.models.UserRoleRepository;
+import kz.ktzh.models.Users;
+import kz.ktzh.models.Authorities;
+import kz.ktzh.models.UserGroups;
 import kz.ktzh.service.UserDAO;
 import kz.ktzh.service.UserService;
 
@@ -29,6 +47,18 @@ public class UserServiceTest {
 
 	@MockBean
 	PasswordEncoder passwordEncMock;
+	
+	@MockBean
+	UserInfoRepository userInfoMock;
+
+	@MockBean
+	UserRepository userRepoMock;
+
+	@MockBean
+	UserRoleRepository userRoleMock;
+
+	@MockBean
+	UserGroupsRepository userGroupsMock;
 
 	@Spy
 	UserService userSpy;
@@ -36,28 +66,93 @@ public class UserServiceTest {
 	@Autowired
 	UserService userImpl;
 
+	private String teststring;
+	private String hashedstring;
+	private Users testuser;
+	private UserInfo testuserinfo;
+	private UserGroups testusergroups;
+	private Authorities testauth;
+	private List<Users> testuserlist;
+
+	@Before
+	public void init() {
+		teststring = "1";
+		hashedstring = "c4ca4238a0b923820dcc509a6f75849b";
+		testuserinfo = new UserInfo(1, "1", "1", "1", "1", new Date(),"1", 1, 1, 1, 1, 1, 1, "1",	"1", "1");
+		testusergroups = new UserGroups(1,1);
+		testauth = new Authorities("1", "1");
+		testuser = new Users("1", "1", "1", 1, 1, "1","1", (byte) 1);
+		testuserlist = new ArrayList<Users>();	
+	}
+	
 	@Test
-	public void testGetSecretQuestion() {
-		when(userDaoMock.getSecretQuestion("user")).thenReturn("1");
-		assertEquals("1", userImpl.getSecretQuestion("user"));
+	public void testRegUserCorrect() {
+		when(userRepoMock.findByUsername(Mockito.anyString())).thenReturn(testuserlist);
+		when(passwordEncMock.encode(Mockito.anyString())).thenReturn("password");
+		when(userRepoMock.save(Mockito.any(Users.class))).thenReturn(testuser);
+		when(userInfoMock.save(Mockito.any(UserInfo.class))).thenReturn(testuserinfo);
+		when(userRoleMock.save(Mockito.any(Authorities.class))).thenReturn(testauth);
+		when(userGroupsMock.save(Mockito.any(UserGroups.class))).thenReturn(testusergroups);		
+		assertEquals("1", userImpl.regUser(testuser, testuserinfo));
+	}
+	
+	@Test
+	public void testRegUserWrong() {
+		testuserlist.add(testuser);
+		when(userRepoMock.findByUsername(Mockito.anyString())).thenReturn(testuserlist);
+		when(passwordEncMock.encode(Mockito.anyString())).thenReturn("password");
+		when(userRepoMock.save(Mockito.any(Users.class))).thenReturn(testuser);
+		when(userInfoMock.save(Mockito.any(UserInfo.class))).thenReturn(testuserinfo);
+		when(userRoleMock.save(Mockito.any(Authorities.class))).thenReturn(testauth);
+		when(userGroupsMock.save(Mockito.any(UserGroups.class))).thenReturn(testusergroups);		
+		assertEquals("0", userImpl.regUser(testuser, testuserinfo));
+	}
+	
+	@Test(expected = Exception.class)
+	public void testRegUserError() {		
+		when(userRepoMock.findByUsername(Mockito.anyString())).thenReturn(testuserlist);
+		when(passwordEncMock.encode(Mockito.anyString())).thenReturn("password");
+		when(userRepoMock.save(Mockito.any(Users.class))).thenThrow(Exception.class);
+		assertEquals("3", userImpl.regUser(testuser, testuserinfo));	
 	}
 
 	@Test
-	public void testChangePassword() {
+	public void testGetSecretQuestionCorrect() {
+		when(userDaoMock.getSecretQuestion("user")).thenReturn("1");
+		assertEquals("1", userImpl.getSecretQuestion("user"));
+	}
+		
+
+	@Test
+	public void testChangePasswordCorrect() {
 		when(passwordEncMock.matches("password", "password")).thenReturn(true);
 		when(userDaoMock.getPassword("user")).thenReturn("password");
 		doNothing().when(userDaoMock).resetPassword("user", "password", "password");
 		assertEquals("1", userImpl.changePassword("user", "password", "password"));
 	}
+	
+	@Test
+	public void testChangePasswordWrong() {
+		when(passwordEncMock.matches("wrongpassword", "password")).thenReturn(true);
+		when(userDaoMock.getPassword("user")).thenReturn("password");
+		doNothing().when(userDaoMock).resetPassword("user", "password", "password");
+		assertEquals("0", userImpl.changePassword("user", "password", "password"));
+	}	
 
 	@Test
-	public void testResetPassword() throws NoSuchAlgorithmException {
-		// TODO : нагуглить как мокать if condition
-		doReturn("1").when(userSpy).md5String("1");
-		when(userDaoMock.getAnswer("user")).thenReturn("1");
-		doNothing().when(userDaoMock).resetPassword("user", "password", "1");
-		when(passwordEncMock.encode("password")).thenReturn("password");
-		assertEquals("1", userImpl.resetPassword("user", "1", "password"));
+	public void testResetPasswordCorrect() throws NoSuchAlgorithmException {		
+		when(userDaoMock.getAnswer(Mockito.anyString())).thenReturn(hashedstring);
+		doNothing().when(userDaoMock).resetPassword("user", "password", "password");
+		when(passwordEncMock.encode(Mockito.anyString())).thenReturn("password");
+		assertEquals("1", userImpl.resetPassword("user", teststring, "password"));
+	}
+	
+	@Test
+	public void testResetPasswordWrong() throws NoSuchAlgorithmException {		
+		when(userDaoMock.getAnswer(Mockito.anyString())).thenReturn("1");
+		doNothing().when(userDaoMock).resetPassword("user", "password", "password");
+		when(passwordEncMock.encode(Mockito.anyString())).thenReturn("password");
+		assertEquals("0", userImpl.resetPassword("user", teststring, "password"));
 	}
 
 }
