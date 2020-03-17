639925dd68a39791214dc4fc12981b3dfdbc1961
Моторнов Кирилл <Motornov_K@railways.kz>
2020-03-17 17:41:25 +0600

    added boommenu

--- lib-mob-client/app/src/main/java/kz/lib_mob_client/MainActivity.java
+++ lib-mob-client/app/src/main/java/kz/lib_mob_client/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.nightonke.boommenu.BoomButtons.HamButton;
+import com.nightonke.boommenu.BoomButtons.SimpleCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
+
 public class MainActivity extends AppCompatActivity {
+
+   BoomMenuButton bmb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BoomMenuButton bmb = findViewById(R.id.bmb);
+        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
+            HamButton.Builder builder = new HamButton.Builder()
+                    .normalImageRes(R.drawable.ic_launcher_foreground)
+                    .normalTextRes(R.string.app_name)
+                    .subNormalTextRes(R.string.app_name);
+            bmb.addBuilder(builder);
+        }
     }
 }
