c553e11af5bbf09f662bfa3a39c7c3fe5da53b52
Моторнов Кирилл <Motornov_K@railways.kz>
2020-03-18 17:03:22 +0600

    changes in menu

--- lib-mob-client/app/src/main/java/kz/lib_mob_client/MainActivity.java
+++ lib-mob-client/app/src/main/java/kz/lib_mob_client/MainActivity.java
@@ -1,28 +1,59 @@
 package kz.lib_mob_client;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.nightonke.boommenu.BoomButtons.HamButton;
+import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
 import com.nightonke.boommenu.BoomButtons.SimpleCircleButton;
 import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.Util;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
    BoomMenuButton bmb;
+   RegulatoryFragment regfrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        regfrag = new RegulatoryFragment();
         BoomMenuButton bmb = findViewById(R.id.bmb);
-        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
-            HamButton.Builder builder = new HamButton.Builder()
-                    .normalImageRes(R.drawable.ic_launcher_foreground)
-                    .normalTextRes(R.string.app_name)
-                    .subNormalTextRes(R.string.app_name);
-            bmb.addBuilder(builder);
-        }
+        bmb.addBuilder(new HamButton.Builder()
+                .normalImageRes(R.drawable.binder)
+                .normalTextRes(R.string.MenuItem1)
+                .normalColor(Color.parseColor("#FF82B1FF"))
+                .imageRect(new Rect(40, 40, Util.dp2px(50), Util.dp2px(50)))
+                .listener(new OnBMClickListener() {
+                    @Override
+                    public void onBoomButtonClick(int index) {
+                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.frgmCont, regfrag);
+                        fragmentTransaction.commit();
+                    }}));
+        bmb.addBuilder(new HamButton.Builder()
+                .normalImageRes(R.drawable.book)
+                .normalTextRes(R.string.MenuItem2)
+                .imageRect(new Rect(40, 40, Util.dp2px(50), Util.dp2px(50)))
+                .normalColor(Color.parseColor("#FF82B1FF")));
+        bmb.addBuilder(new HamButton.Builder()
+                .normalImageRes(R.drawable.film)
+                .normalTextRes(R.string.MenuItem3)
+                .imageRect(new Rect(40, 40, Util.dp2px(50), Util.dp2px(50)))
+                .normalColor(Color.parseColor("#FF82B1FF")));
+        bmb.addBuilder(new HamButton.Builder()
+                .normalImageRes(R.drawable.circular)
+                .normalTextRes(R.string.MenuItem4)
+                .imageRect(new Rect(40, 40, Util.dp2px(50), Util.dp2px(50)))
+                .normalColor(Color.parseColor("#FF82B1FF")));
+
     }
+
 }
