d8cb13bea4a0f793617ba115bd92a26e0f1327fc
jurgen <bfbc1986@bk.ru>
2020-05-06 16:14:03 +0500

    menu changes

--- lib-mob-client/app/src/main/java/kz/lib_mob_client/MainActivity.java
+++ lib-mob-client/app/src/main/java/kz/lib_mob_client/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.nightonke.boommenu.BoomButtons.HamButton;
@@ -18,10 +19,10 @@ import com.nightonke.boommenu.Util;
 import kz.lib_mob_client.fragments.RegulatoryMenuFragment;
 import kz.lib_mob_client.fragments.RegulatoryDocumentationFragment;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-   RegulatoryMenuFragment regfrag;
-   RegulatoryDocumentationFragment rdf;
+    RegulatoryMenuFragment regfrag;
+    RegulatoryDocumentationFragment rdf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +41,8 @@ public class MainActivity extends AppCompatActivity{
                         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                         fragmentTransaction.replace(R.id.frgmCont, regfrag);
                         fragmentTransaction.commit();
-                    }}));
+                    }
+                }));
         bmb.addBuilder(new HamButton.Builder()
                 .normalImageRes(R.drawable.book)
                 .normalTextRes(R.string.MenuItem2)
@@ -56,16 +58,56 @@ public class MainActivity extends AppCompatActivity{
                 .normalTextRes(R.string.MenuItem4)
                 .imageRect(new Rect(40, 40, Util.dp2px(50), Util.dp2px(50)))
                 .normalColor(Color.parseColor("#FF82B1FF")));
+        bmb.addBuilder(new HamButton.Builder()
+                .normalImageRes(R.drawable.circular)
+                .normalTextRes(R.string.placeholder)
+                .imageRect(new Rect(40, 40, Util.dp2px(50), Util.dp2px(50)))
+                .normalColor(Color.parseColor("#FF82B1FF")));
+        bmb.addBuilder(new HamButton.Builder()
+                .normalImageRes(R.drawable.circular)
+                .normalTextRes(R.string.placeholder)
+                .imageRect(new Rect(40, 40, Util.dp2px(50), Util.dp2px(50)))
+                .normalColor(Color.parseColor("#FF82B1FF")));
+
 
     }
 
-    public void onStudyGuidesClick(View v){
+    public void onRegMenuBtnClick(View v) {
         rdf = new RegulatoryDocumentationFragment();
         Bundle args = new Bundle();
-        args.putInt("arg_category", 19);
+
+        switch (v.getId()) {
+            case R.id.lawsbtn:
+                args.putInt("arg_category", 1);
+                break;
+            case R.id.codebtn:
+                args.putInt("arg_category", 2);
+                break;
+            case R.id.regbtn:
+                args.putInt("arg_category", 3);
+                break;
+            case R.id.devprogtn:
+                args.putInt("arg_category", 4);
+                break;
+            case R.id.decreebtn:
+                args.putInt("arg_category", 5);
+                break;
+            case R.id.intdocbtn:
+                args.putInt("arg_category", 6);
+                break;
+        }
+        //args.putInt("arg_category", 1);
         rdf.setArguments(args);
         displayFragment(rdf);
     }
+
+   /* public void onCodeBtnClick(View v){
+        rdf = new RegulatoryDocumentationFragment();
+        Bundle args = new Bundle();
+        args.putInt("arg_category", 2);
+        rdf.setArguments(args);
+        displayFragment(rdf);
+    }*/
 
     private void displayFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
